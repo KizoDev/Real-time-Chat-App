53b483851dbd790cfa5659b3b4b6745f2aff5fc9
mayaclaudia <mayaammuclaudia@gmail.com>
2024-03-03 05:16:37 -0800

    complete express integration

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-import { createServer } from "http";
+import express from "express";
 import {Server} from "socket.io";
+import  path  from "path";
+import { fileURLToPath } from "url";
 
-const httpServer = createServer()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const io = new Server(httpServer, {
+const app = express()
+
+app.use(express.static(path.join(__dirname, "Public")))
+const port = process.env.PORT || 3500
+
+const expressServer = app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+
+})
+const io = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV ==="production" ? false :
          ["http://localhost:5500", "http://127.0.0.1:5500"]
@@ -19,6 +31,4 @@ io.on('connection', socket => {
         io.emit("message", `${socket.id.substring(0, 5) }:${data} `)
     })
 })
-httpServer.listen(3500, () => console.log('listening on port 3500')
 
-)
