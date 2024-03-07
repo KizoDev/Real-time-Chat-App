69d126d263c8c4dd867328b227e732469a7229bc
mayaclaudia <mayaammuclaudia@gmail.com>
2024-03-07 03:52:57 -0800

    complete activity

--- server/index.js
+++ server/index.js
@@ -6,10 +6,11 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const port = process.env.PORT || 3500
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, "Public")))
-const port = process.env.PORT || 3500
 
 const expressServer = app.listen(port, () => {
     console.log(`listening on port ${port}`)
@@ -25,10 +26,27 @@ const io = new Server(expressServer, {
 
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`);
+   // upon connection to user only
+   socket.emit('message', 'welcome to chat app')
 
+   //upon connection ... to other users
+   socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}}connected`)
+
+    // listening to message event
     socket.on('message', data => {
         console.log(data);
         io.emit("message", `${socket.id.substring(0, 5) }:${data} `)
+    })
+
+    //when user disconnect.. to others
+    socket.on('disconnect',() => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}} 
+        disconnected`)
+    } )
+
+    //listening for activity
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity ', name)
     })
 })
 
